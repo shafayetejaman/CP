4d97cadbbda30ad7e17741e912590b11df993d16
shafayetejaman <shafaitzaman@gmail.com>
2025-10-10 15:33:17 +0600

    Add solutions for HackerRank problems: Solve Me First, Average Permutation, Hotel Bytelandia, Mighty Friend, and Trace of Matrix.

---
+++ template.py
@@ -0,0 +1,21 @@
+from collections import deque, defaultdict, Counter
+from bisect import bisect_left as lowerBound, bisect_right as upperBound
+import math
+
+
+def inputer():
+    N = int(input())
+    arr = list(map(int, input().split()))
+    return N, arr
+
+
+def main():
+    N, arr = inputer()
+    ans = None
+
+    return ans
+
+
+t = int(input())
+for _ in range(t):
+    print(main())
